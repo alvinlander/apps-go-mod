fa5d5221639d9fc9d696cfa61aaccce0d53c3b33
alvinlander <landeralvin@gmail.com>
2021-09-29 21:05:30 +0700

    Using Module Learn-go-mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alvinlander/apps-go-mod
+
+go 1.17
+
+require github.com/alvinlander/learn-go-mod/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	dev_go_mod "github.com/alvinlander/learn-go-mod/v2"
+)
+
+func main() {
+	fmt.Println(dev_go_mod.SayHello("Lans"))
+}
